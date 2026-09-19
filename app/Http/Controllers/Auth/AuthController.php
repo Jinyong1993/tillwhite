b9eb5529e40b9dc9ce555bc170a24ef1711a9910
jeongjinyoung <rhffpq12@naver.com>
2026-09-20 00:18:39 +0900

    비밀번호 검증

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -22,6 +23,13 @@ class AuthController extends Controller
 
         // 입력한 아이디와 일치하는 사용자가 없는 경우
         if (!$user) {
+            return response()->json([
+                'message' => '아이디 또는 비밀번호가 올바르지 않습니다.',
+            ], 401);
+        }
+
+        // 입력한 비밀번호와 DB에 저장된 암호화된 비밀번호 비교
+        if (!Hash::check($request->password, $user->password)) {
             return response()->json([
                 'message' => '아이디 또는 비밀번호가 올바르지 않습니다.',
             ], 401);
