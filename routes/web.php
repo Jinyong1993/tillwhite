c9b2341760724462a1ad09a238fbd0e3bea60193
jeongjinyoung <rhffpq12@naver.com>
2026-09-19 23:36:38 +0900

    순서 변경

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
-// 로그인
-Route::post('/tillwhite/login', [AuthController::class, 'login']);
-
 // 기본 페이지
 Route::get('/tillwhite/{path?}', fn () => view('tillwhite'))->where('path', '.*');
+
+// 로그인
+Route::post('/tillwhite/login', [AuthController::class, 'login']);
