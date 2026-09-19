b97e10f3f1cf9a6c115a936529169730979f7a42
jeongjinyoung <rhffpq12@naver.com>
2026-09-19 22:26:24 +0900

    로그인 화면 수정

--- resources/js/pages/LoginPage.vue
+++ resources/js/pages/LoginPage.vue
@@ -1,7 +1,19 @@
 <template>
   <v-container>
-    <v-btn color="primary">
-      로그인
-    </v-btn>
+    <v-card
+      class="mx-auto"
+      width="400"
+      subtitle="BAKERY - 생산 & 폐기 입력"
+    >
+      <template v-slot:title>
+        <span class="font-weight-black">Till White </span>
+      </template>
+
+      <v-card-text class="bg-surface-light pt-4">
+        <v-btn color="primary">
+          로그인
+        </v-btn>
+      </v-card-text>
+    </v-card>
   </v-container>
 </template>
