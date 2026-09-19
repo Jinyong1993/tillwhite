41acc0d0e31a6e5820abd3d5aca6436ec80e7458
jeongjinyoung <rhffpq12@naver.com>
2026-09-19 23:39:17 +0900

    정리

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -2,20 +2,20 @@ import { createRouter, createWebHistory } from 'vue-router';
 import LoginPage from '../pages/Auth/LoginPage.vue';
 
 const routes = [
-    {
-        path: '/tillwhite',
-        redirect: '/tillwhite/login',
-    },
-    {
-        path: '/tillwhite/login',
-        name: 'login',
-        component: LoginPage,
-    },
+  {
+    path: '/tillwhite',
+    redirect: '/tillwhite/login',
+  },
+  {
+    path: '/tillwhite/login',
+    name: 'login',
+    component: LoginPage,
+  },
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
+  history: createWebHistory(),
+  routes,
 });
 
 export default router;
