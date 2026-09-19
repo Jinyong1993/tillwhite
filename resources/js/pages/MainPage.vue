fa6a80a44f54f3641a6040368f8891706ccd255a
jeongjinyoung <rhffpq12@naver.com>
2026-09-20 00:52:21 +0900

    메인 화면 추가

---
+++ resources/js/pages/MainPage.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container>
+    <v-card
+      class="mx-auto"
+      width="400"
+    >
+      <v-card-title
+        class="font-weight-black"
+      >
+        Till White
+      </v-card-title>
+
+      <v-card-subtitle
+        class="mb-3"
+      >
+        BAKERY - 생산 & 폐기 입력
+      </v-card-subtitle>
+
+      <v-card-text
+        class="bg-surface-light"
+      >
+        <v-btn
+          @click="$router.back()"
+          block
+        >
+          뒤로가기
+        </v-btn>
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
+
+<script setup>
+
+</script>
