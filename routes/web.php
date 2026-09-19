c927b04ae01c48b4976e0d847b4cda5af2100b8f
jeongjinyoung <rhffpq12@naver.com>
2026-09-19 23:35:56 +0900

    루트 수정, 로그인 관련 추가

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/{any?}', function () {
-    return view('welcome');
-})->where('any', '.*');
+// 로그인
+Route::post('/tillwhite/login', [AuthController::class, 'login']);
+
+// 기본 페이지
+Route::get('/tillwhite/{path?}', fn () => view('tillwhite'))->where('path', '.*');
