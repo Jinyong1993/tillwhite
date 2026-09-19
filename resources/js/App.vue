b319413dda92c53846229f17c7b287f4e5be9e36
jeongjinyoung <rhffpq12@naver.com>
2026-09-19 21:08:01 +0900

    Initial Laravel Vue Vuetify setup

---
+++ resources/js/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-app>
+        <v-main>
+            <v-container>
+                <v-card class="pa-6">
+                    <v-card-title>Tillwhite 생산관리</v-card-title>
+                    <v-card-text>
+                        Vue + Vuetify 연결 성공
+                    </v-card-text>
+                </v-card>
+            </v-container>
+        </v-main>
+    </v-app>
+</template>
---
+++ resources/js/app.js
@@ -0,0 +1,15 @@
+import './bootstrap';
+
+import { createApp } from 'vue';
+import { createVuetify } from 'vuetify';
+
+import 'vuetify/styles';
+import '@mdi/font/css/materialdesignicons.css';
+
+import App from './App.vue';
+
+const vuetify = createVuetify();
+
+createApp(App)
+    .use(vuetify)
+    .mount('#app');
