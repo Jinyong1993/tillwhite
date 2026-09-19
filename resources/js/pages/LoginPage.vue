580083c14f292da1bb56906ece6c75076759936f
jeongjinyoung <rhffpq12@naver.com>
2026-09-19 23:11:22 +0900

    로그인 함수 추가

--- resources/js/pages/LoginPage.vue
+++ resources/js/pages/LoginPage.vue
@@ -20,16 +20,19 @@
         class="bg-surface-light"
       >
         <v-text-field
+          v-model="id"
           label="아이디"
           variant="outlined"
         />
 
         <v-text-field
+          v-model="password"
           label="비밀번호"
           type="password"
           variant="outlined"
         />
         <v-btn
+          @click="login()"
           block
         >
           로그인
@@ -38,3 +41,17 @@
     </v-card>
   </v-container>
 </template>
+
+<script setup>
+  import { ref } from 'vue';
+
+  // 로그인 관련
+  const id = ref('');
+  const password = ref('');
+
+  // 로그인 버튼 클릭시
+  function login() {
+    console.log(id.value);
+    console.log(password.value);
+  }
+</script>
